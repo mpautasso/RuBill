9ce75aca6e0f9a917911c4d422993428ca7995d8
Mauricio Pautasso <mauricio.pautasso@gmail.com>
2011-06-10 18:25:06 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubill::Application.config.secret_token = 'e02bec0a0f93bc067af1ad4ca114c4d0035dec266fee863a09769e312aa3a83e6954626f96989ced8e05e92f9b0f2f636fa158ddf4e90b68e7d412ce4fb59911'
