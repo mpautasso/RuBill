652221cc4e4bbcbe079f7a7a40f273065dbfa74e
Franco Brusatti <fbrusatti@gmail.com>
2011-06-22 18:08:08 -0300

    Add basic style to site

--- config/navigation.rb
+++ config/navigation.rb
@@ -3,17 +3,22 @@ SimpleNavigation::Configuration.run do |navigation|
 
   # Define the primary navigation
   navigation.items do |primary|
-    primary.item :users, 'Users', users_path
+    if current_user
+      primary.item :item, 'Home', root_path, :class => 'None' do |tickets|
 
-    primary.item :invoices, 'Invoices', invoices_path do |invoices|
-      invoices.item :items, 'Items', items_invoices_path
+        primary.item :users, 'Users', users_path
+
+        primary.item :invoices, 'Invoices', invoices_path do |invoices|
+          invoices.item :items, 'Items', items_invoices_path
+        end
+
+        primary.item :rates, 'Rates', rates_path
+        primary.item :incomming_calls, 'Incomming Calls', incomming_calls_path
+        primary.item :outgoing_calls, 'Outgoing Calls', outgoing_calls_path
+
+        primary.item :logout, 'Logout', destroy_user_session_url
+      end
     end
-    primary.item :rates, 'Rates', rates_path
-    primary.item :incomming_calls, 'Incomming Calls', incomming_calls_path
-    primary.item :outgoing_calls, 'Outgoing Calls', outgoing_calls_path  
-
-
-
   end
 
 end
