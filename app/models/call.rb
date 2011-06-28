1f11b571643efbd6011e3fa79dd88a3dd10d929f
Franco Brusatti <fbrusatti@gmail.com>
2011-06-28 00:25:17 -0300

    Fix issue with device relations in call model

--- app/models/call.rb
+++ app/models/call.rb
@@ -1,5 +1,6 @@
 class Call < ActiveRecord::Base
   has_many :items, :as => :reference
-  belongs_to :outgoing_device, :primary_key => "src", :foreign_key => "device_id"
-  belongs_to :incomming_device, :primary_key => "dst", :foreign_key => "device_id"
+
+  belongs_to :outgoing_device, :class_name => 'Device', :primary_key => "src", :foreign_key => "device_id"
+  belongs_to :incomming_device, :class_name => 'Device', :primary_key => "dst", :foreign_key => "device_id"
 end
