03c1d9618491f712fc875df6727a93f84a8daf7c
Mauricio Pautasso <mauricio.pautasso@gmail.com>
2011-07-25 20:56:59 -0300

    Fix foreign keys for device model

--- app/models/device.rb
+++ app/models/device.rb
@@ -15,9 +15,9 @@
 class Device < ActiveRecord::Base
   belongs_to :user
 
-  has_many :outgoing_calls, :primary_key => :exten
-  has_many :incomming_calls
-  has_many :failed_calls
+  has_many :outgoing_calls, :foreign_key => :src, :primary_key => :exten
+  has_many :incomming_calls, :foreign_key => :src, :primary_key => :exten
+  has_many :failed_calls, :foreign_key => :src, :primary_key => :exten
  
   validates :exten, :presence => true,
                     :uniqueness => true
