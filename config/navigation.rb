af3d20c3cab56720871eca07d33fd242143cc5f6
Mauricio Pautasso <mauricio.pautasso@gmail.com>
2011-08-02 18:43:53 -0300

    Styling the popme headers and puts text in search text field

--- config/navigation.rb
+++ config/navigation.rb
@@ -12,20 +12,16 @@ SimpleNavigation::Configuration.run do |navigation|
         primary.item :devices, 'Devices', devices_path
       end
 
+      primary.item :rates, 'Rates', rates_path
+      
       if current_user.device || current_user.admin?
-        primary.item :invoices, 'Consult', consults_path
-
-        primary.item :invoices, 'Saved Expenses', invoices_path
-
         primary.item :outgoing_calls, 'Outgoing Calls', outgoing_calls_path
-        
         primary.item :incomming_calls, 'Incomming Calls', incomming_calls_path
-        
         primary.item :failed_calls, 'Failed Calls', failed_calls_path
+        primary.item :invoices, 'Consult', consults_path
+        primary.item :invoices, 'Saved Expenses', invoices_path
       end
       
-      primary.item :rates, 'Rates', rates_path
-
       primary.item :logout, 'Logout', destroy_user_session_url 
     end
   end
