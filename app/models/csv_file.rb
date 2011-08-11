0e45e0b15e0f3c372d6ec132f70bb41da6f96cca
Mauricio <mauricio.pautasso@gmail.com>
2011-08-11 18:00:50 -0300

    Delete file_name field from model

--- app/models/csv_file.rb
+++ app/models/csv_file.rb
@@ -1,7 +1,16 @@
 class CsvFile < ActiveRecord::Base
-  attr_accessible :user_id, :csv, :file_name
+  attr_accessible :user_id, :csv
 
   belongs_to :user
 
-  has_attached_file :csv
+  has_attached_file :csv#, :path => ":rails_root/public/system/csvs/:style.:extension"
+
+  def self.search(search)
+    if search
+      where('csv_file_name LIKE ?', "%#{search}%")
+    else
+      scoped
+    end
+  end
+  
 end
