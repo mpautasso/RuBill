afaf72e1678606906930dd5edf3e650035033090
Mauricio <mauricio.pautasso@gmail.com>
2011-08-12 19:21:23 -0300

    Add validations to the csv models

--- app/models/csv_file.rb
+++ app/models/csv_file.rb
@@ -2,8 +2,15 @@ class CsvFile < ActiveRecord::Base
   attr_accessible :user_id, :csv
 
   belongs_to :user
-
-  has_attached_file :csv#, :path => ":rails_root/public/system/csvs/:style.:extension"
+  
+  validates :csv_file_name, :uniqueness => true
+  validates_attachment_presence :csv
+  validates_attachment_content_type :csv, :content_type => ['application/x-crossover-csv']
+  
+  has_attached_file :csv,
+                    :path => ":rails_root/public/system/csvs/:basename.:extension",
+                    :url => "/system/csvs/:basename.:extension"
+  
 
   def self.search(search)
     if search
